b2524c4f258ba4a2117a9177e0d7e1bd6cf1c97f
naidusaladi <79361491+naidusaladi@users.noreply.github.com>
2023-01-30 13:22:38 +0530

    Add files via upload

---
+++ creation.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np 
+import streamlit as st 
+import pickle 
+
+def Prediction(arr):
+    model=pickle.load(open("C:\\Users\\DELL\\Desktop\\stress_model_streamlit\\model.sav", 'rb'))
+    pred=model.predict([arr])
+    return pred
+def main():
+    st.title("Human Stress Prediction")
+    Heart_rate=st.text_input("Heart rate (60-160) (in BPM) :")
+    
+    Temperature=st.text_input("Temperature (25-50) (in celcius) :")
+    output=""
+    if st.button("Predict Stress Level"):
+        try:
+            Heart_rate=int(Heart_rate)
+            Temperature=int(Temperature)
+            output=Prediction([Heart_rate,Temperature])
+            st.success(output[0])
+        except:
+            st.write("Enter correct values")
+       
+
+
+if __name__=="__main__":
+    main()
+    
